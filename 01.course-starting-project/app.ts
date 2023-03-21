05c690eb057331418581656ac8dab2475231dcc9
SJ.Lee <eagleduk@gmail.com>
2023-03-21 22:47:35 +0900

    기본 변수 타입
    기본 변수 타입 정리

---
+++ 01.course-starting-project/app.ts
@@ -0,0 +1,27 @@
+enum ROLE {
+  ADMIN,
+  GUEST = 100,
+}
+
+const person: {
+  name: string; // 문자열 타입
+  age: number; // 숫자 티입
+  male: boolean; // 불리언 타입
+  role: ROLE; // ROLE 타입
+  address: string[]; // Array(string) 타입
+  etc: [number, string]; // Tuple([number, string]) 타입
+  description: any;
+} = {
+  name: "tomas",
+  age: 34,
+  male: true,
+  role: ROLE.ADMIN,
+  address: ["street", "54-center"],
+  etc: [33, "CENTER"],
+  description: 30,
+};
+
+person.description = "Number And String abled property.";
+
+if (person.role === ROLE.GUEST) {
+}
